4f09edd933aba1934b91e0bc90f24dc2e37a0b31
reaperizy <wadi.wahyudin7@gmail.com>
2022-06-11 13:11:12 +0700

    menambahkan file chatbot + res

---
+++ android/app/src/main/kotlin/com/example/chatbot_beras/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatbot_beras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:chatbot_beras/chatbot.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.brown,
+      ),
+      home: const ChatBotScreen(),
+    );
+  }
+}
+
+  // theme: ThemeData(
+  //   colorScheme: ColorScheme(
+  //     brightness: Brightness.dark,
+  //     surface: Colors.yellow,
+  //     onSurface: Colors.black,
+  //    // Colors that are not relevant to AppBar in DARK mode:
+  //     primary: Colors.grey,
+  //     onPrimary: Colors.grey,
+  //     primaryVariant: Colors.grey,
+  //     secondary: Colors.grey,
+  //     secondaryVariant: Colors.grey,
+  //     onSecondary: Colors.grey,
+  //     background: Colors.grey,
+  //     onBackground: Colors.grey,
+  //     error: Colors.grey,
+  //     onError: Colors.grey,
+  //   ),
+  //),
